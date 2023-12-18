4d8adfb9fc47eb6c49e5031e11e36159100abaf4
shorinami <shorinami@atombuilt.com>
2023-12-18 13:47:08 +0200

    chore: Add respective copyright in files

--- src/main/kotlin/AtomShulkersPlugin.kt
+++ src/main/kotlin/AtomShulkersPlugin.kt
@@ -1,3 +1,13 @@
+/*
+ * Copyright © AtomBuilt 2023. All rights reserved.
+ *
+ * This software has been developed by AtomBuilt.
+ * Visit our website at https://atombuilt.com.
+ * For inquiries, please contact us at contact@atombuilt.com.
+ *
+ * All rights to this software are held by the respective owners.
+ */
+
 package com.atombuilt.shulkers
 
 import com.atombuilt.atomkt.spigot.KotlinPlugin
